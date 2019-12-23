3b320c1dd560219fd386bc3c366e9bb0303d84f5
Qiming Wu <3370392@myuwc.ac.za>
2019-12-23 08:08:56 +0200

    1st Upload
    First upload of uploader script.

---
+++ upload-text.py
@@ -0,0 +1,51 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+import sys
+import threading
+import inspect
+
+class ProgressPercentage(object):
+    def __init__(self, filename, ):
+        self._filename = filename
+        seeker = inspect.stack()[1][4][0]
+        lastSlash = filename.rfind('/')
+
+        if("upload_file" in seeker):
+            try:
+                self._size = float(os.path.getsize(filename))
+            except FileNotFoundError:
+                print("Local File not found.")
+                exit()
+        elif("download_file" in seeker):
+            try:
+                self._size = float(boto3.resource('s3').Bucket(filename[:lastSlash]).Object(filename[lastSlash+1:]).content_length)
+            except ClientError as e:
+                print("Client Error:", e.response['ResponseMetadata']['HTTPStatusCode'], e.response['Error']['Message'])
+                exit()
+        else:
+            print("Invalid stack.")
+            exit()
+
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            if(bytes_amount!=0):
+                #sys.stdout.write("\r%s  %s / %s  (%.2f%%)" % (self._filename, self._seen_so_far, self._size,percentage))
+                print("\r%s  %s / %s  (%.2f%%)" % (self._filename, self._seen_so_far, self._size,percentage),end='',flush=True)
+            else:
+                print()
+
+s3 = boto3.client('s3')
+s3.upload_file("Linux_Academy.html", "qwuamz-upload-test", "Linux_Academy.html", Callback=ProgressPercentage("Linux_Academy.html"))
+s3.download_file("qwuamz-upload-test", "Linux_Academy.html", "Linux_Academy.html", Callback=ProgressPercentage("qwuamz-upload-test/Linux_Academy.html"))
+
+#s3 = boto3.resource('s3')
+#object_summary = s3.ObjectSummary('qwuamz-upload-test','Linux_Academy.html')
+#print(object_summary)
+#print(boto3.resource('s3').Bucket('qwuamz-upload-test').Object('Linux_Academy.html').content_length)
